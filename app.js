bcb345a364504137c5456bb00c9560d5e8450f3b
Kyle Robert van Winkoop <kylevanwinkoop@gmail.com>
2022-01-05 16:42:07 -0800

    Adding comments

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 
+//Getting the express and server packages
+
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 var fs = require('fs');
 
+
+//Including a express.json()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Getting the properties Json file
 const config = require('./properties.json');
 
-
+//Setting up the 'Get' Request: it will return index.html
 app.get('/',function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+
+//Setting up the 'post' request: it will return the parameters in the request body
 app.post('/',function(req, res){
     
     console.log(req.body)
 	res.send(req.body);
 });
 
-app.param('name', function(req, res, next, name) {
-  console.log("GOT IT");
-  console.log(req.body);
-  console.log(name);
-  next();
-});
-
-
+//Activating the server
 serv.listen(config.properties.port);
 console.log("Server started.");
